16aa433b44cbc0f3c574768a520c531d2db3d184
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-04-15 15:54:54 -0600

    Validar que usuario actual este activo

--- main.py
+++ main.py
@@ -58,6 +58,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     return user
 
 
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
 @app.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_dict = fake_users_db.get(form_data.username)
