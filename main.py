a836bdfa046229e478e31fa515cf3cac546c20ce
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-04-15 15:43:53 -0600

    Agregando el modelo para el usuario

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
 
 @app.get("/items/")
