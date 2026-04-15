5f4cd700407e68ad9d3a26e44f50feec188ec309
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-04-15 15:49:49 -0600

    Mock: Base de datos hardcodeada

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
 
 class User(BaseModel):
     username: str
