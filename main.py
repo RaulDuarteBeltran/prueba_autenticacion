a833c7bd86ade666be895103b086a474b0e01f21
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2026-04-18 18:56:26 -0600

    Add func to create access tokens

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
+import jwt
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pwdlib import PasswordHash
@@ -77,6 +79,17 @@ def authenticate_user(fake_db, username: str, password: str):
     if not verify_password(password, user.hashed_password):
         return False
     return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 # async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
